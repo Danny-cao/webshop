0196d8d3396d92e9cdcefb2298d26878080a0847
HUDannyCao <danny.cao@student.hu.nl>
2019-03-22 16:28:48 +0100

    persistence DAO toegevoegd

---
+++ src/main/java/persistence/OracleBaseDao.java
@@ -0,0 +1,26 @@
+package persistence;
+
+import java.sql.*;
+
+
+public class OracleBaseDao {
+
+    private static final String DB_DRIV = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_URL = "jdbc:oracle:thin:@//ondora04.hu.nl:8521/EDUC21";
+    private static final String DB_USER = "cursist";
+    private static final String DB_PASS = "cursist4491";
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName(DB_DRIV).newInstance();
+        }
+        catch (InstantiationException | IllegalAccessException | ClassNotFoundException e1) {
+            e1.printStackTrace();
+        }
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+
+    public void closeConnection(Connection conn) throws SQLException{
+        conn.close();
+    }
+}
