5c1d2f2e40c3e329eac568695de213ef04d573fe
Berend Sliedrecht <berend.sliedrecht@student.hu.nl>
2019-03-23 22:15:33 +0100

    Cleaned frontend.

--- src/main/webapp/js/shoppingcart-page.js
+++ src/main/webapp/js/shoppingcart-page.js
@@ -8,7 +8,6 @@ $('.edit').click(function(){
 	var getPrice = 30.00;
 	var amount = $(".amount",$(this).parent().parent()).val();
 	subtotal = amount * getPrice;
-	console.log(subtotal);
 	$(".subtotal",$(this).parent().parent()).text(subtotal);
 	subtotal = 0;
 	calculateTotal();
@@ -21,4 +20,14 @@ function calculateTotal(){
 		total += isNaN(stval) ? 0 : stval;
 	});
 	$('#total').text(total);
+};
+
+function calculateSubtotal() {
+	var subtotal = 0;
+	var getPrice = 30.00;
+	var amount = $(".amount",$(".edit").parent().parent()).val();
+	subtotal = amount * getPrice;
+	$(".subtotal",$(".edit").parent().parent()).text(subtotal);
+	subtotal = 0;
+	calculateTotal();
 };
