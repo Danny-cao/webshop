f852e0c0f27a3851a34a9528f4f02da0d4ee2f79
HUDannyCao <danny.cao@student.hu.nl>
2019-03-27 17:18:26 +0100

    sale per product id

---
+++ src/main/java/persistence/AccountPostgreSQLDAOImpl.java
@@ -0,0 +1,11 @@
+package persistence;
+
+public class AccountPostgreSQLDAOImpl extends PostgreSQLBaseDao implements AccountDAO{
+
+	@Override
+	public boolean validateLogin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
--- src/main/java/persistence/SalePostgreSQLDAOImpl.java
+++ src/main/java/persistence/SalePostgreSQLDAOImpl.java
@@ -2,25 +2,50 @@ package persistence;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Category;
 import model.Product;
 import model.Sale;
 
 public class SalePostgreSQLDAOImpl extends PostgreSQLBaseDao implements SaleDAO{
+	
+	ProductPostgreSQLDAOImpl pobd = new ProductPostgreSQLDAOImpl();
 
 	@Override
 	public Sale findByProduct(Product product) {
-		return null;
+		
+        try (Connection con = super.getConnection()) {
+
+            String query = "select * from aanbieding where product = ?";
+            PreparedStatement stmt = con.prepareStatement(query);
+            stmt.setInt(1, product.getId());
+            ResultSet dbResultSet = stmt.executeQuery();
+            
+            
+            if(dbResultSet.next() == false) {
+            	return null;
+            }
+
+            Date begin = dbResultSet.getDate("vandatum");
+            Date end = dbResultSet.getDate("totdatum");
+            float price = dbResultSet.getFloat("prijs");
+            
+            return new Sale(product, begin, end, price);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
 	}
 
 	@Override
 	public List<Sale> findAll() {
 		
-		ProductPostgreSQLDAOImpl pobd = new ProductPostgreSQLDAOImpl();
 		
 		List<Sale> sales = new ArrayList<>();
 		
